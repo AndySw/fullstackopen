a044851724cb37f6627f53c6d278301737b1eb9a
Andy Sweetman <adsweetman@gmail.com>
2025-01-09 14:15:28 +0000

    AS - Committing exercise 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -12,20 +12,36 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
 
   const handleGoodOnClick = () => {
     console.log('handleGoodOnClick')
+    setAll(all + 1)
     setGood(good + 1)
   }
 
   const handleNeutralOnClick = () => {
     console.log('handleNeutralOnClick')
+    setAll(all + 1)
     setNeutral(neutral + 1)
   }
 
   const handleBadOnClick = () => {
     console.log('handleBadOnClick')
+    setAll(all + 1)
     setBad(bad + 1)
+  }
+
+  const getAverageScore = () => {
+    const averageScore = ((good * 1) + (neutral * 0.5) + (bad * -1)) / all
+    const result = Number.isNaN(averageScore) ? '' : averageScore.toFixed(2)
+    return result
+  }
+
+  const getPositivePercent = () => {
+    const positivePercent = (good / all) * 100
+    const result = Number.isNaN(positivePercent) ? '' : '%' + positivePercent.toFixed(2)
+    return result
   }
 
   return (
@@ -38,6 +54,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {getAverageScore()}</p>
+      <p>positive {getPositivePercent()}</p>
     </div>
   )
 }
