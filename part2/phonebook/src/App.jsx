565b75e709f4e1eaeb3e4480f3c805b8e004ba8f
Andy Sweetman <adsweetman@gmail.com>
2025-02-13 09:18:24 +0000

    AS - Exercise 2.6 complete

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -5,18 +5,35 @@ function App() {
   const [persons, setPersons] = useState([{ name: 'Arto Hellas'}])
   const [newName, setNewName] = useState('')
 
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personArray = [{
+      name: newName
+    }]
+
+    console.log(personArray)
+
+    setPersons(persons.concat(personArray))
+
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
       <form>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={e => setNewName(e.target.value)} />
         </div>
         <div>
-          <button type="submit">add</button>
+          <button type="submit" onClick={addPerson}>add</button>
         </div>
       </form>
       <h2>Numbers</h2>
+      
+      {persons.map(person => (
+        <div>{person.name}</div>
+      ))}
+
     </div>
   )
 }
